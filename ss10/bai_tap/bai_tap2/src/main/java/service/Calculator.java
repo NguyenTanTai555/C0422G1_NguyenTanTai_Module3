a40a062796406f57ce890d94486c4bfdb8403ba4
unknown <you@example.com>
2022-07-28 22:40:26 +0700

    update

---
+++ ss10/bai_tap/bai_tap2/src/main/java/service/Calculator.java
@@ -0,0 +1,21 @@
+package service;
+
+public class Calculator {
+    public double calculate(double number1, double number2, String operator) throws ArithmeticException {
+        double result = 0;
+        if (operator.equals("*")){
+            result = number1 * number2;
+        }else if(operator.equals("/")){
+            if (number2 != 0){
+                result = number1 / number2;
+            } else {
+                throw new ArithmeticException("An exception occurred: / by zero ");
+            }
+        }else if(operator.equals("+")){
+            result = number1 + number2;
+        }else if(operator.equals("-")){
+            result = number1 - number2;
+        }
+        return result;
+    }
+}
---
+++ ss10/bai_tap/bai_tap2/src/main/java/unit/ArithmeticException.java
@@ -0,0 +1,9 @@
+package unit;
+
+public class ArithmeticException extends Exception {
+        private String message;
+        public ArithmeticException(String message){
+            super(message);
+
+}
+}
